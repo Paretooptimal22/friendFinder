e763234b2c9e19c31b47c09be61cf173af7d0e6b
yokevi <you.k2208@gmail.com>
2019-11-29 18:14:16 -0800

    added middleware to server.js

--- server.js
+++ server.js
@@ -2,4 +2,8 @@ const express = require(`express`)
 
 const app = express()
 
+app.use(express.static(__dirname, `public`))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
 app.listen(process.env.PORT || 3000)
