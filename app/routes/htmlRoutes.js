5f84ab9931533b1232353d977d8fdb7dc96a1f66
yokevi <you.k2208@gmail.com>
2019-12-01 17:48:15 -0800

    fixed link and button to survey page

--- app/routes/htmlRoutes.js
+++ app/routes/htmlRoutes.js
@@ -4,4 +4,8 @@ module.exports = app => {
   app.get(`/`, (req, res) => {
     res.sendFile(join(__dirname, `../public/home.html`))
   })
+
+  app.get(`/survey`, (req, res) => {
+    res.sendFile(join(__dirname, `../public/survey.html`))
+  })
 }
