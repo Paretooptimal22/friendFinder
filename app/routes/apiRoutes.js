35ca27b480899a3a1f41f0e896c7857caa6d5d99
yokevi <you.k2208@gmail.com>
2020-01-28 15:09:45 -0800

    changed color scheme and added api routes

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -1,5 +1,17 @@
 const path = require(`path`)
 
 module.exports = app => {
+  // GET ALL FRIENDS
+  app.get('/friends', (req, res) => {
+    res.json(friends)
+  })
+
+  // POST A FRIEND
+  app.post('/friends', (req, res) => {
+    friends.push(req.body)
+    res.sendStatus(200)
+  })
+
   
+
 }
